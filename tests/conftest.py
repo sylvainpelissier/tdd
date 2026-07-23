2cd0e43094b9f8602fa9bc004f2ea964b016653c
sylvainpelissier <sylvain.pelissier@gmail.com>
2026-07-23 21:20:04 +0200

    Rename module to tddoc

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,5 +36,5 @@ def keychain():
     """
     Load the internal keychain with bundled certificates.
     """
-    from tdd.keychain import internal
+    from tddoc.keychain import internal
     return internal(include_test=True, check_expiry=False)
